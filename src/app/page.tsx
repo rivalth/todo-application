1fc7bfa95e978e27ad67ccc490011ad4fa1d27cb
Rivalth <106491824+rivalth@users.noreply.github.com>
2024-12-16 22:41:14 +0300

    add Toaster component and Button UI component; update global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,144 @@
-import Image from "next/image";
+"use client";
+
+import { TaskItem } from "@/components/taskItem";
+import { Button } from "@/components/ui/button";
+import { GithubBrandIcon, TrashIcon } from "@/utils/icons";
+import { cn } from "@/utils/mergeClassName";
+import { Span } from "next/dist/trace";
+import { FormEvent, useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export type Task = {
+    id: string;
+    value: string;
+    completed: boolean;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const [taskInput, setTaskInput] = useState("");
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const createTask = (e: FormEvent) => {
+        "use client";
+        e.preventDefault();
+        const inputValue = taskInput.trim();
+        // clear the input field
+        setTaskInput("");
+        if(inputValue === "") {
+            toast.error("You can't create an empty task");
+            return;
+        }
+
+        // we cant use the index of the array because if the user deletes a task the index will change and we'll have a bug of duplicated ids
+        // however the id will always be unique if we randomly generate it
+        const id = "_" + Math.random().toString(36).substr(2, 9);
+
+        const newTask: Task = {
+            id,
+            value: inputValue,
+            completed: false,
+        };
+
+        // create the task
+        setTasks((prev) => [newTask, ...prev]);
+
+        toast.success("Task created successfully");
+    };
+
+    const changeCompilation = (id: string) => {
+        "use client";
+        setTasks((prev) => {
+            return prev.map((t) =>
+                t.id === id ? { ...t, completed: !t.completed } : t
+            );
+        });
+        toast.info(`Task status changed to ${tasks.find((t) => t.id === id)?.completed ? "completed" : "uncompleted"}`);
+    };
+
+    const deleteTask = (id: string) => {
+        "use client";
+        setTasks(tasks.filter((t) => t.id !== id));
+        toast.error("Task deleted");
+    };
+
+    const updateTaskValue = (id: string, value: string) => {
+        "use client";
+        setTasks((prev) => {
+            return prev.map((t) => (t.id === id ? { ...t, value } : t));
+        });
+        toast.info("Task text has been updated");
+    };
+
+    useEffect(() => {
+        // load the tasks from the local storage
+        const tasks = localStorage.getItem("tasks");
+        if (tasks) {
+            setTasks(JSON.parse(tasks));
+        }
+    }, []);
+
+    useEffect(() => {
+        // save the tasks to the local storage
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] max-w-screen-sm mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 justify-start h-full">
+                <h1 className="text-4xl font-bold text-center">
+                    Managing Your Tasks
+                </h1>
+
+                <form
+                    onSubmit={createTask}
+                    className="flex gap-4 items-center bg-zinc-100 inputShadow rounded-full outline-none focus:outline-orange-400"
+                >
+                    <input
+                        type="text"
+                        className="bg-transparent text-black placeholder:text-zinc-600 w-full h-full pl-4 py-3 outline-none rounded-full"
+                        placeholder="Enter your task"
+                        value={taskInput}
+                        onChange={(e) => setTaskInput(e.target.value)}
+                    />
+                    <Button
+                        className="mr-2 h-[80%] text-xs relative group bg-zinc-700 text-white px-4 rounded-full"
+                        type="submit"
+                    >
+                        Create Task
+                    </Button>
+                </form>
+
+                <div>
+                    <h2 className="text-2xl font-bold">Tasks</h2>
+                    {tasks.length === 0 && (
+                        <p className="text-gray-500 min-h-12 h-full w-full text-center justify-center flex items-center">
+                            You don't have any tasks yet
+                        </p>
+                    )}
+                    <ul className="flex flex-col gap-4 mt-4">
+                        {tasks.map((task: Task) => (
+                            <TaskItem
+                                key={task.id}
+                                task={task}
+                                changeCompilation={changeCompilation}
+                                deleteTask={deleteTask}
+                                updateTaskValue={updateTaskValue}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            </main>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://github.com/rivalth/todo-application"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <GithubBrandIcon />
+                    This Websie is Open Source
+                </a>
+            </footer>
+        </div>
+    );
 }
---
+++ src/components/taskItem.tsx
@@ -0,0 +1,128 @@
+import { Task } from "@/app/page";
+import { cn } from "@/utils/mergeClassName";
+import { useRef, useState } from "react";
+import { motion, PanInfo, useMotionValue } from "motion/react";
+import { CompleteTaskIcon, TrashIcon } from "@/utils/icons";
+export const TaskItem = ({
+    task,
+    changeCompilation,
+    deleteTask,
+    updateTaskValue,
+}: {
+    task: Task;
+    changeCompilation: (id: string) => void;
+    deleteTask: (id: string) => void;
+    updateTaskValue: (id: string, value: string) => void;
+}) => {
+    const x = useMotionValue(0);
+    const [taskActive, setTaskActive] = useState<boolean>(false);
+    const [taskDragMission, setTaskDragMission] = useState<
+        "none" | "compilation" | "deletion"
+    >("none");
+
+    const handleDragEnd = () => {
+        if (taskDragMission === "compilation") {
+            changeCompilation(task.id);
+        }
+
+        if (taskDragMission === "deletion") {
+            deleteTask(task.id);
+        }
+
+        setTaskActive(false);
+        setTaskDragMission("none");
+    };
+
+    const handleOnDrag = (event: MouseEvent, info: PanInfo) => {
+        const value = x.get();
+        console.log("value", value);
+        if (value > 15) {
+            setTaskDragMission("deletion");
+        } else if (value < -10) {
+            setTaskDragMission("compilation");
+        } else {
+            setTaskDragMission("none");
+        }
+    };
+
+    return (
+        <li
+            key={task.id}
+            className="flex items-center justify-between gap-2 relative group"
+        >
+            <div
+                className={cn(
+                    "absolute left-16 top-0 h-full flex items-center justify-center gap-2 transition-all duration-200",
+                    taskActive && "-left-20"
+                )}
+            >
+                <button className="text-blue-500">
+                    <CompleteTaskIcon />
+                </button>
+            </div>
+            <motion.div
+                drag="x"
+                style={{
+                    x,
+                }}
+                dragConstraints={{ left: 0, right: 0 }} // Sürükleme sınırları
+                dragElastic={0.1} // Esneklik
+                dragMomentum={false} // Momentum kapalı
+                onDragStart={() => {
+                    setTaskActive(true);
+                }} // Sürükleme başladığında görevi aktif yap
+                onPanEnd={() => {
+                    x.set(0);
+                }}
+                onDragEnd={handleDragEnd} // Sürükleme bırakılınca işlemleri yap
+                onDrag={handleOnDrag} // Drag mesafesini kaydet
+                whileTap={{
+                    cursor: "grabbing",
+                    boxShadow: "0px 1px 10px #00000020",
+                }} // Tıklanınca büyüt
+                className={cn(
+                    "flex items-center w-full bg-zinc-50 p-2 rounded-md relative z-[2] cursor-pointer outline outline-zinc-400/20 transition-outline",
+                    taskDragMission == "compilation" && "outline-blue-500",
+                    taskDragMission == "deletion" && "outline-red-500",
+                )}
+            >
+                <button
+                    className="inline-flex items-center mr-1 gap-2 border border-zinc-400 rounded-full w-6 h-6 justify-center cursor-pointer outline-none"
+                    onClick={() => changeCompilation(task.id)}
+                >
+                    {task.completed && (
+                        <div className="rounded-full w-4 h-4 bg-black" />
+                    )}
+                </button>
+                <span
+                    contentEditable
+                    suppressContentEditableWarning={true}
+                    ref={(el) => {
+                        if (el) {
+                            el.innerText = task.value; // Initialize content
+                        }
+                    }}
+                    onBlur={(e) => {
+                        const value = e.currentTarget.innerText.trim(); // Avoid leading/trailing spaces
+                        if (value !== task.value) {
+                            updateTaskValue(task.id, value);
+                        }
+                    }}
+                    className="flex-1 outline-none p-1 rounded transition-all duration-200"
+                >
+                    {task.value}
+                </span>
+            </motion.div>
+            <div
+                className={cn(
+                    "absolute right-16 top-0 h-full flex items-center justify-center gap-2 transition-all duration-200",
+                    taskActive && "-right-20"
+                )}
+            >
+                <button className="text-red-500">
+                    <TrashIcon />
+                </button>
+            </div>
+        </li>
+    );
+};
